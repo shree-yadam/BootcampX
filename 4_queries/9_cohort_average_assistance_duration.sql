527151f2a247a6bdf6460507256242e24fbffddb
Shree Devi Yadam <shree.yadam@gmail.com>
2021-06-22 18:10:21 +0000

    added 4th round of queries

---
+++ 4_queries/9_cohort_average_assistance_duration.sql
@@ -0,0 +1,6 @@
+SELECT AVG(result.total_duration) as average_total_duration
+  FROM (SELECT cohorts.name as cohort, SUM(completed_at - started_at) as total_duration
+ FROM assistance_requests
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN cohorts ON cohorts.id = students.cohort_id
+ GROUP BY cohorts.name) as result;
